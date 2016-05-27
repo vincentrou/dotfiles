37f591db4148019d70e56d38e150818d2e85667a
Vincent Rousseau <vincent.rousseau@irstea.fr>
2016-05-27 16:53:11 +0200

    Set indent size to 2 spaces in qtcreator

--- script/edit_ini.py
+++ script/edit_ini.py
@@ -6,12 +6,23 @@ home = os.path.expanduser('~')
 config= ConfigParser.RawConfigParser()
 config.optionxform = str
 config.read(home + '/.config/QtProject/QtCreator.ini')
+
 try:
     config.add_section('CppCodeStyleSettings')
-    config.set('CppCodeStyleSettings','LegacyTransformed','true')
 except ConfigParser.DuplicateSectionError:
     pass
-
+config.set('CppCodeStyleSettings','LegacyTransformed','true')
 config.set('CppCodeStyleSettings','CurrentPreferences','@ByteArray(codingstyle)')
+
+try:
+    config.add_section('textTabPreferences')
+except ConfigParser.DuplicateSectionError:
+    pass
+config.set('textTabPreferences','AutoSpacesForTabs','false')
+config.set('textTabPreferences','IndentSize','2')
+config.set('textTabPreferences','PaddingMode','1')
+config.set('textTabPreferences','SpacesForTabs','true')
+config.set('textTabPreferences','TabSize','2')
+
 with open(home + '/.config/QtProject/QtCreator.ini', 'w') as configfile:
     config.write(configfile)
